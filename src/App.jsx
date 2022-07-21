a1d021019574d83f50db2c994fe67770fd492197
Giedrius <messagesbox@outlook.com>
2022-07-21 10:55:05 +0300

    labas zuiki

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import HomeWork from './HomeWork';
+
+function App() {		
+    return (		
+    <div className='App'>
+      <header className='App-header'>
+		   <h1>Labas, zuiki!</h1>
+       <HomeWork></HomeWork>
+      </header>
+    </div>		
+    		
+    )		
+};
+	
+export default App;
---
+++ src/HomeWork.jsx
@@ -0,0 +1,12 @@
+/* Sukurti komponentą, kuris užrašytų “LABAS, ZUIKI!”. Raidžių spalva pink. 
+  Spalva tekstui nurodoma komponento viduje naudojant style. */
+
+function HomeWork() {
+
+    return (
+    <div>LABAS, ZUIKI!</div>
+    )
+
+}
+
+export default HomeWork;
